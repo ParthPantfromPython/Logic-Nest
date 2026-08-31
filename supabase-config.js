1d3cdb8e2d7c5f2848aa1b6edeb3e4cf70de2a47
ParthPantfromPython <141381688+ParthPantfromPython@users.noreply.github.com>
2026-08-31 17:27:49 +0530

    Add Supabase public configuration

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.LOGIC_NEST_SUPABASE = {
+  url: 'https://lpynccsiitfhpqlbbxnf.supabase.co',
+  publishableKey: 'sb_publishable_fo67VmtcDzQ0xCppsYrROg_zxO3Pw7X',
+  adminEmail: 'pantparth021@gmail.com'
+};
